677d20d48174d1f24c25b5503318175cd6871199
SeanJaii <seanjaii.t@gmail.com>
2016-07-20 22:00:32 +0100

    add random motivation selector

--- app/index.js
+++ app/index.js
@@ -2,10 +2,18 @@
 const express = require('express');
 const app = express();
 
-// setup first route
+// require: motivations & pick-one
+const motivations = require('motivations');
+const pickOne = require('pick-one');
+
 app.get('/hello', function(request, response){
     console.log("request to /hello");
     response.send("Hello!");
 });
+
+app.get('/', function(req, res){
+    res.send(pickOne(motivations));
+});
+// use pick one
 
 module.exports = app;
